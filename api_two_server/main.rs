12bade61180e0d8cb31b6ae8506775cbab9639f1
wennhmann <info@wennhmann.de>
2025-02-05 16:05:46 +0100

    two server actix

--- api_two_server/main.rs
+++ api_two_server/main.rs
@@ -1,6 +1,7 @@
 //# actix-rt = "1.0"
 //# actix-web = "2.0"
 //# futures = "0.3"
+//is a solution from: https://stackoverflow.com/questions/59642576/run-multiple-actix-app-on-different-ports
 use actix_web::{web, App, HttpServer, Responder};
 use futures::future;
 
@@ -17,7 +18,7 @@ async fn main() -> std::io::Result<()> {
     let s1 = HttpServer::new(move || {
             App::new().service(web::scope("/utils").route("/one", web::get().to(utils_one)))
         })
-        .bind("0.0.0.0:3006")?
+        .bind("0.0.0.0:8081")?
         .run();
     let s2 = HttpServer::new(move || {
             App::new().service(web::resource("/health").route(web::get().to(health)))
